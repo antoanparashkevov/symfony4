84aa9559e6cedfa9e220efb00be8aca52731521a
Antoan Parashkevov <tonyparashkevov@gmail.com>
2021-05-05 01:17:37 +0300

    Second commit

---
+++ assets/Form.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-5">Create your offer</h1>
+    </div>
+    
+</div>
+
+</template>
+
+<script>
+export default {
+    name:"Form",
+    data(){
+    return{
+        firstName:"Antoan",
+    };
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Entity/Category.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $end_price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getEndPrice(): ?int
+    {
+        return $this->end_price;
+    }
+
+    public function setEndPrice(int $end_price): self
+    {
+        $this->end_price = $end_price;
+
+        return $this;
+    }
+}
