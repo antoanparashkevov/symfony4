82f59e7d9016eeb848123e08a229653ecc2a9c64
Boris Sanchez | ThinkPad E470 | Debian 9 <sanchobor@gmail.com>
2021-05-05 16:37:18 +0300

    added apache-pack in composer.json

--- assets/Form.vue
+++ assets/Form.vue
@@ -1,9 +1,8 @@
 <template>
 <div class="jumbotron">
     <div class="container">
-        <h1 class="display-5">Create your offer</h1>
+        <h1 class="display-5">Create your offer - great progress</h1>
     </div>
-    
 </div>
 
 </template>
